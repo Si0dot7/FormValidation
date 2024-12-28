e4a87b07ff034e657416bbbb1759a1615b35b06f
Si0dot7 <felm27010@gmail.com>
2024-12-28 16:18:28 +0700

    last update

--- src/Components/FormComponent.js
+++ src/Components/FormComponent.js
@@ -13,34 +13,47 @@ const FormComponent = () =>{
     const [errorPassword,setErrorPassword] = useState('')
     const [errorRePassword,setErrorRePassword] = useState('')
 
+    const [userColor,setUserColor] = useState('')
+    const [emailColor,setEmailColor] = useState('')
+    const [passwordColor,setPasswordColor] = useState('')
+    const [rePasswordColor,setRePasswordColor] = useState('')
+    
     const validateForm = (e) =>{
         e.preventDefault() /*ไม่รีเช็ตข้อมูลเมื่อรีหน้าเว็บ จะเก็บข้อมูลไว้ */
         if(userName.length >= 8){
             setErrorUserName('')
+            setUserColor('Green')
         }
         else{
             setErrorUserName('ป้อนชื่อผู้ใช้มากกว่าหรือเท่ากับ 8ตัวอักษร')
+            setUserColor('Red')
         }
 
         if(email.includes('@')){
             setErrorEmail('')
+            setEmailColor('Green')
         }
         else{
             setErrorEmail('รูปแบบอีเมลไม่ถูกต้อง')
+            setEmailColor('Red')
         }
 
         if(password.length >= 8){
             setErrorPassword('')
+            setPasswordColor('Green')
         }
         else{
             setErrorPassword('รหัสผ่านต้องมากกว่าหรือเท่ากับ 8ตัวอักษร')
+            setPasswordColor('Red')
         }
 
-        if(repassword == password){
+        if((repassword === password) && (repassword.length !== 0)){
             setErrorRePassword('')
+            setRePasswordColor('Green')
         }
         else{
-            setErrorRePassword('รหัสผ่านไม่ตรงกัน')
+            setErrorRePassword('รหัสผ่านไม่ตรงกัน หรือ ไม่มีรหัสผ่าน')
+            setRePasswordColor('Red')
         }
     }
     return(
@@ -49,26 +62,26 @@ const FormComponent = () =>{
                 <h2>แบบฟอร์มลงทะเบียน</h2>
                 <div className = 'form-control'> 
                     <label>ชื่อผู้ใช้</label>
-                    <input type = 'text' value = {userName} onChange = {(e)=>setUserName(e.target.value)}/>
-                    <small>{erroruserName}</small>
+                    <input type = 'text' value = {userName} onChange = {(e)=>setUserName(e.target.value)} style = {{borderColor:userColor}}/>
+                    <small style = {{color: 'red'}}>{erroruserName}</small>
                 </div>
                 
                 <div className = 'form-control'>
                     <label>อีเมล</label>
-                    <input type = 'text' value = {email} onChange = {(e)=>setEmail(e.target.value)}></input>
-                    <small>{errorEmail}</small>
+                    <input type = 'text' value = {email} onChange = {(e)=>setEmail(e.target.value)} style = {{borderColor:emailColor}}></input>
+                    <small style = {{color:'red'}}>{errorEmail}</small>
                 </div>
 
                 <div className = 'form-control'>
                     <label>รหัสผ่าน</label>
-                    <input type = 'text' value = {password} onChange = {(e)=>setPassword(e.target.value)}></input>
-                    <small>{errorPassword}</small>
+                    <input type = 'password' value = {password} onChange = {(e)=>setPassword(e.target.value)} style = {{borderColor:passwordColor}}></input>
+                    <small style = {{color:'red'}}>{errorPassword}</small>
                 </div>
 
                 <div className = 'form-control'>
                     <label>ยืนยันรหัสผ่าน</label>
-                    <input type = 'text' value = {repassword} onChange = {(e)=>setRePassword(e.target.value)}></input>
-                    <small>{errorRePassword}</small>
+                    <input type = 'password' value = {repassword} onChange = {(e)=>setRePassword(e.target.value)} style = {{borderColor:rePasswordColor}}></input>
+                    <small style = {{color:'red'}}>{errorRePassword}</small>
                 </div>
 
                 <button type = 'submit'>ลงทะเบียน</button>
